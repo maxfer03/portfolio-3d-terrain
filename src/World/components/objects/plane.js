f3499cdbcaf665a0526514ed6b43633a50162a62
maxfer03 <sandsferdinand@gmail.com>
2022-01-21 11:50:40 -0300

    added temporary python server

--- src/World/components/objects/plane.js
+++ src/World/components/objects/plane.js
@@ -8,25 +8,22 @@ import {
 
 function createPlane(color = "white", yOffset = 0, wire = false, transparent = false) {
   const loader = new TextureLoader();
-  const textureUrl =
-    "https://images.unsplash.com/photo-1603077747884-b458237de736?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80";
   const heightUrl =
-    "https://upload.wikimedia.org/wikipedia/commons/8/8b/PerlinNoise2d.png";
+    "http://localhost:8000/assets/textures/height.png";
     const alphaUrl =
-    "https://cdnb.artstation.com/p/assets/images/images/017/608/583/large/alexander-pham-t-point-of-light.jpg";
-  let texture = loader.load(textureUrl);
-  let height = loader.load(heightUrl);
+    "http://localhost:8000/assets/textures/alpha.png"
+    let height = loader.load(heightUrl);
   let alpha = loader.load(alphaUrl);
 
 
-  const geometry = new PlaneBufferGeometry(40, 40, 64, 64);
+  const geometry = new PlaneBufferGeometry(50, 50, 64, 64);
 
   let material = new MeshStandardMaterial({
     color,
     //map: texture,
     //flatShading: true,
     displacementMap: height,
-    displacementScale: 4,
+    displacementScale: 3,
     wireframe: wire,
     wireframeLinewidth: 2,
     transparent,
