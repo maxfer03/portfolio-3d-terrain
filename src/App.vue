1c3034cdf2d7ca8019a53c2b88919f54a7fb07de
maxfer03 <sandsferdinand@gmail.com>
2022-02-24 16:11:58 -0300

    adding hero

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
-  <router-view/>
+  <section>
+    <Navbar/>
+    <router-view/>
+  </section>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/Presentation.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="title-container hero-left-content">
+            <h1 class="hero-title">Max Fernandez</h1>
+            <div class="hero-subtitle">
+              <span> > </span>
+              <p class="hero-subtitle-text">Web developer</p>
+            </div>
+          </div>
+</template>
+
+<script>
+export default {
+    name: 'Presentation'
+}
+</script>
+
+<style lang='scss'>
+@import "@/assets/styles/_variables.scss";
+@import "@/assets/styles/_mixins.scss";
+
+</style>
