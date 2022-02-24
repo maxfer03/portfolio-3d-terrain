c62e37b3f91f0481460960c9ac4f7bce6ec3ab18
maxfer03 <sandsferdinand@gmail.com>
2022-02-24 15:50:45 -0300

    initial stylings

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
 </template>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap");
 #app {
   font-family: "Space Mono", monospace;
   -webkit-font-smoothing: antialiased;
