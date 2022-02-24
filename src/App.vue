5791ff07ca6c1a0ddffbe83a3183e6482d29ced3
maxfer03 <sandsferdinand@gmail.com>
2022-02-24 15:37:10 -0300

    migrating portfolio to vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-view/>
+</template>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap");
+#app {
+  font-family: "Space Mono", monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color:white;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    Home screen
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
